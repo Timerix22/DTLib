ef6a55b3859155356e026634af52796d2e549589
Timerix <timerix11@gmail.com>
2021-09-13 17:03:59 +0300

    new mutex wrapper wiht autorelease

---
+++ SafeMutex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace DTLib
+{
+    public class SafeMutex
+    {
+        readonly Mutex Mutex = new();
+        bool isReleased = false;
+
+        // тут выполняется отправленный код
+        public void Execute(Action action, out Exception exception)
+        {
+            try
+            {
+                exception = null;
+                Mutex.WaitOne();
+                action();
+                Mutex.ReleaseMutex();
+                isReleased = true;
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+                if (!isReleased) Mutex.ReleaseMutex();
+            }
+        }
+    }
+}
