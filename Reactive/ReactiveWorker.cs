a55d71973ba5658644a70a43d29d28ee2a6dd6ba
Timerix <timerix11@gmail.com>
2021-10-03 00:17:31 +0300

    reactivity

---
+++ Reactive/ReactiveWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTLib.Reactive
+{
+    public abstract class ReactiveWorker<T>
+    {
+        List<ReactiveStream<T>> Streams = new();
+
+        SafeMutex StreamCollectionAccess = new();
+
+        public void Join(ReactiveStream<T> stream) => StreamCollectionAccess.Execute(()=>Streams.Add(stream));
+        public void Leave(ReactiveStream<T> stream) => StreamCollectionAccess.Execute(() => Streams.Remove(stream));
+    }
+}
