fe35153278bf55370093ef123713db686039fd32
Timerix22 <timerix11@gmail.com>
2022-01-14 00:12:40 +0300

    now it's not a single project, but a solution

---
+++ TestProgram/DtsodV2X/TestDtsodV23.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading;
+using DTLib.Dtsod;
+using DTLib.Experimental;
+using static TestProgram.Program;
+
+namespace TestProgram.DtsodV2X;
+
+public static class TestDtsodV23
+{
+    public static void TestAll()
+    {
+        TestBaseTypes();
+        TestLists();
+        TestComplexes();
+        TestReSerialization();
+        TestSpeed();
+        TestMemoryConsumption();
+    }
+
+    public static void TestBaseTypes()
+    {
+        Info.Log("b", "[TestDtsodV23/TestBaseTypes]");
+        DtsodV23 dtsod = new(File.ReadAllText("DtsodV2X\\base_types.dtsod"));
+        foreach (var pair in dtsod)
+            Info.LogNoTime("b", pair.Value.GetType().Name + ' ', "w", pair.Key + ' ', "c", pair.Value.ToString());
+        Info.Log("g", "[test completed]");
+    }
+    public static void TestLists()
+    {
+        Info.Log("b", "[TestDtsodV23/TestLists]");
+        DtsodV23 dtsod = new(File.ReadAllText("DtsodV2X\\lists.dtsod"));
+        foreach (var pair in dtsod)
+        {
+            Info.LogNoTime("b",  pair.Value.GetType().Name + ' ', "w", pair.Key, "c",
+                $" count: {pair.Value.Count}");
+            foreach (var el in pair.Value)
+                Info.LogNoTime("b", '\t'+el.GetType().Name + ' ', "c", el.ToString());
+        }
+        Info.Log("g", "[test completed]");
+    }
+
+    public static void TestComplexes()
+    {
+        Info.Log("b", "[TestDtsodV23/TestComplexes]");
+        DtsodV23 dtsod = new(File.ReadAllText("DtsodV2X\\complexes.dtsod"));
+        foreach (var pair in dtsod)
+        {
+            Info.LogNoTime("b", pair.Value.GetType().Name + ' ', "w", pair.Key, 
+                "b", " length: ", "c", pair.Value.Keys.Count.ToString() + "\n\t",
+                "y", pair.Value.ToString().Replace("\n","\n\t"));
+        }
+        Info.Log("g", "[test completed]");
+    }
+
+    public static void TestReSerialization()
+    {
+        Info.Log("b", "[TestDtsodV23/TestReSerialization]");
+        DtsodV23 dtsod = new DtsodV23(new DtsodV23(new DtsodV23(
+            new DtsodV23(File.ReadAllText("DtsodV2X\\complexes.dtsod")).ToString()).ToString()).ToString());
+        Info.Log("y", dtsod.ToString());
+        Info.Log("g", "[test completed]");
+    }
+
+    public static void TestSpeed()
+    {
+        Info.Log("b", "[TestDtsodV23/TestSpeed]");
+        IDtsod dtsod=null;
+        string text = File.ReadAllText("DtsodV2X\\messages.dtsod");
+        Tester.LogOperationTime("V21 deserialization",100,()=>dtsod=new DtsodV21(text));
+        Tester.LogOperationTime("V21 serialization", 100, () => _=dtsod.ToString());
+        Tester.LogOperationTime("V23 deserialization", 100, () => dtsod = new DtsodV23(text));
+        Tester.LogOperationTime("V23 serialization", 100, () => _ = dtsod.ToString());
+        Info.Log("g", "[test completed]");
+    }
+
+    public static void TestMemoryConsumption()
+    {
+        Info.Log("b", "[TestDtsodV23/TestMemoryConsumption]");
+        string text = File.ReadAllText("DtsodV2X\\messages.dtsod");
+        var a = GC.GetTotalMemory(true);
+        DtsodV23[] dtsods = new DtsodV23[100];
+        for (int i = 0; i < dtsods.Length; i++)
+            dtsods[i] = new(text);
+        var b = GC.GetTotalMemory(true);
+        Info.Log("b", "at the start: ","c",$"{a/1024} kb\n", 
+            "b", "at the end: ", "c", $"{b / 1024} kb\n{dtsods.Count()}","b"," dtsods initialized");
+        Info.Log("g", "[test completed]");
+    }
+}
---
+++ TestProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using DTLib;
+global using DTLib.Extensions;
+global using DTLib.Filesystem;
+using DTLib.Dtsod;
+using DTLib.Loggers;
+using TestProgram.DtsodV2X;
+
+namespace TestProgram;
+
+static class Program
+{
+    public static DefaultLogger Info = new();
+    static public void Main()
+    {
+
+        Info.Enable();
+        PublicLog.LogEvent += Info.Log;
+        PublicLog.LogNoTimeEvent += Info.LogNoTime;
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.Title="tester";
+        try
+        {
+            TestDtsodV23.TestAll();
+        }
+        catch (Exception ex)
+        { Info.Log("r", ex.ToString()); }
+        Console.ResetColor();
+    }
+}
