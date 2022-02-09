67e21bef891b11e504b184ce51ad1a9f7d55749a
Timerix22 <timerix11@gmail.com>
2022-02-09 23:48:03 +0300

    cosmopolitan removed

--- DtsodC/README.md
+++ DtsodC/README.md
@@ -5,20 +5,9 @@ DtsodV23 parser in C# works too slow, so i wrote V24 parser in C
 <br>
 
 
-## Compiling on Linux
+## Building on Linux
 **Required packages:** gcc
 
-Compile with glibc: 
-```bash
-make std_build
-````
-
-Compile with <a href="https://github.com/jart/cosmopolitan">cosmopolitan<a> (crossplatform libc implementation): 
 ```bash
 make build
-```
-
-If you see the `run-detectors: unable to find an interpreter` error, just execute this command:
-```bash
-sudo echo ':APE:M::MZqFpD::/bin/sh:' >/proc/sys/fs/binfmt_misc/register
-```
+````
--- DtsodC/src/base/std.h
+++ DtsodC/src/base/std.h
@@ -1,19 +1,14 @@
 #pragma once
-
-#if COSMOPOLITAN
-    #include "../../cosmopolitan/cosmopolitan.h"
-#else
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <stdint.h>
-    #include <stdbool.h>
-    #include <locale.h>
-    #include <uchar.h>
-    #include <wchar.h>
-    #include <sys/stat.h>
-    #include <time.h>
-    #include <unistd.h>
-#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <locale.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
 #define CHOOSE(B, Y, N) __builtin_choose_expr(B, Y, N)
 #define IFTYPE(X, T) __builtin_types_compatible_p(typeof(X), T)
