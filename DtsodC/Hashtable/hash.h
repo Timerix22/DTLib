7f653adfd289e8917b21d3686ff58d3bf5013d2e
Timerix22 <timerix11@gmail.com>
2022-03-18 20:42:39 +0300

    DtsodC moved to separate repo

--- DtsodC/Hashtable/hash.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../base/base.h"
-
-//djb2 hash function from http://www.cse.yorku.ca/~oz/hash.html
-uint32 ihash(char *str);
-//sdbm hash function
-uint64 lhash(char* str);
