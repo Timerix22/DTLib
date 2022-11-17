73979336011c8eabfc23afcf2c5e6ef10eccb629
Timerix22 <timerix11@gmail.com>
2022-11-17 21:19:01 +0600

    added colors to ConsoleLogger

--- DTLib.Logging/Loggers/ConsoleLogger.cs
+++ DTLib.Logging/Loggers/ConsoleLogger.cs
@@ -17,12 +17,21 @@ public class ConsoleLogger : ILogger
     {
         var msg = format.CreateMessage(context, severity, message);
         lock (consolelocker) 
-            ColoredConsole.Write(msg);
+            ColoredConsole.Write(ColorFromSeverity(severity),msg);
     }
     
     public void Log(string context, LogSeverity severity, object message)
         => Log(context, severity, message, Format);
 
+    private static ConsoleColor ColorFromSeverity(LogSeverity severity)
+        => severity switch
+        {
+            LogSeverity.Debug => ConsoleColor.Gray,
+            LogSeverity.Info => ConsoleColor.White,
+            LogSeverity.Warn => ConsoleColor.Yellow,
+            LogSeverity.Error => ConsoleColor.Red,
+            _ => throw new ArgumentOutOfRangeException(nameof(severity), severity, null)
+        };
     
     public void Dispose()
     {
