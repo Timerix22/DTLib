de64155ebd6fdc7072efce5610c0e6fddc117deb
timerix <timerix11@gmail.com>
2022-12-06 02:29:58 +0600

    LoggerContext

--- DTLib.Logging/Global.cs
+++ DTLib.Logging/Global.cs
@@ -1,6 +1,7 @@
 global using System;
 global using System.Collections;
 global using System.Collections.Generic;
+global using System.Runtime.CompilerServices;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
--- DTLib.Logging/LoggerExtensions.cs
+++ DTLib.Logging/LoggerExtensions.cs
@@ -5,20 +5,30 @@ namespace DTLib.Logging.New;
 public static class LoggerExtensions
 {
     // replaces same overload in every ILogger instaance
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Log(this ILogger logger, string context, LogSeverity severity, object message)
         => logger.Log(context, severity, message, logger.Format);
+    
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void LogDebug(this ILogger logger, string context, object message)
     => logger.Log(context, LogSeverity.Debug, message);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void LogInfo(this ILogger logger, string context, object message)
         => logger.Log(context, LogSeverity.Info, message);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void LogWarn(this ILogger logger, string context, object message)
         => logger.Log(context, LogSeverity.Warn, message);
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void LogError(this ILogger logger, string context, object message)
         => logger.Log(context, LogSeverity.Error, message);
     
+    
     /// uses Ben.Demystifier to serialize exception
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void LogException(this ILogger logger, string context, Exception ex)
         => logger.Log(context, LogSeverity.Error, ex.Demystify());
 }
