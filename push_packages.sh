61d062673ce7a727d9c301e8420fd760cf8bc592
Timerix22 <timerix11@gmail.com>
2023-01-04 05:28:46 +0600

    github nuget pushing removed

--- push_packages.sh
+++ push_packages.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-echo enter github api key:
-read -rs GHK
+#echo enter github api key:
+#read -rs GHK
 echo enter nuget api key:
 read -rs NGK
 
 for PACK in $(find ./nuget -name '*.nupkg'); do
-    dotnet nuget push $PACK -k $GHK -s github --skip-duplicate
+    #dotnet nuget push $PACK -k $GHK -s github --skip-duplicate
     dotnet nuget push $PACK -k $NGK -s nuget.org --skip-duplicate
 done
