11e5148cd2b1cfb69c61e7fbba6655083b235f9f
Timerix22 <timerix11@gmail.com>
2023-02-22 00:48:17 +0600

    pack.sh has been rewritten

--- pack.sh
+++ pack.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/bash
-set -ex
-rm -rf nuget_new
-dotnet pack -c Publish -o ./nuget_new/
-rm -rf nuget
-mv nuget_new nuget
-ls nuget
+set -xe
+
+if [[ -d nuget ]]; then
+    mkdir -p nuget_old
+    TIMESTAMP=$(date +%Y.%m.%d_%H-%M-%S)
+    mv nuget "nuget_$TIMESTAMP"
+    tar cvf "nuget_old/nuget_$TIMESTAMP.tar" "nuget_$TIMESTAMP"
+    rm -rf "nuget_$TIMESTAMP"
+    ls -lh nuget_old
+fi
+mkdir nuget
+
+function create_package() {
+    echo "----------[$1]----------"
+    cd "$1" || return 1
+    dotnet pack -c Publish -o bin/pack_tmp || return 1
+    rm -rf bin/pack
+    mv bin/pack_tmp bin/pack
+    cp bin/pack/* ../nuget/
+    cd ..
+}
+
+set +x
+create_package DTLib
+create_package DTLib.Ben.Demystifier
+create_package DTLib.Dtsod
+create_package DTLib.Logging
+create_package DTLib.Network
+
+ls -lh nuget
