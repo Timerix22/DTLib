ddbdc7d558635cf556c05f0916faaaf1aeb96306
Timerix22 <timerix11@gmail.com>
2023-03-15 19:13:42 +0600

    IsExternalInit disabled in new dotnet

--- DTLib/Extensions/FrameworkFix.cs
+++ DTLib/Extensions/FrameworkFix.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel;
 
 // включает init и record из c# 9.0
+#if !NET6_0 && !NET7_0 && !NET8_0
 namespace System.Runtime.CompilerServices;
 
 [EditorBrowsable(EditorBrowsableState.Never)]
 public class IsExternalInit { }
+#endif
