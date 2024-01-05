9d48b41e309e7812a05b3b00ee7d0d96fd4dd16c
Timerix22 <timerix11@gmail.com>
2024-01-06 03:03:24 +0600

    removed useless methods

--- DTLib/Extensions/BaseConverter.cs
+++ DTLib/Extensions/BaseConverter.cs
@@ -37,43 +37,8 @@ public static class BaseConverter
     public static ulong ToULong<T>(this T input) => Convert.ToUInt64(input);
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float ToFloat(this string input) => float.Parse(input, NumberStyles.Any, CultureInfo.InvariantCulture);
-#if NETSTANDARD2_1 || NET6_0 || NET7_0 || NET8_0
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static float ToFloat(this ReadOnlySpan<char> input) => float.Parse(input, NumberStyles.Any, CultureInfo.InvariantCulture);
-#endif
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static double ToDouble<T>(this T input) => Convert.ToDouble(input, CultureInfo.InvariantCulture);
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal ToDecimal<T>(this T input) => Convert.ToDecimal(input, CultureInfo.InvariantCulture);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int ToInt(this byte[] bytes)
-    {
-        int output = 0;
-        for (ushort i = 0; i < bytes.Length; i++)
-            output = output * 256 + bytes[i];
-        return output;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static byte[] IntToBytes(this int num)
-    {
-        List<byte> output = new();
-        while (num != 0)
-        {
-            output.Add((byte)(num % 256));
-            num = (int)(num / 256.0);
-        }
-        output.Reverse();
-        return output.ToArray();
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int TruncateToInt(this double number) => Math.Truncate(number).ToInt();
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int TruncateToInt(this decimal number) => Math.Truncate(number).ToInt();
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static long TruncateToLong(this double number) => Math.Truncate(number).ToLong();
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static long TruncateToLong(this decimal number) => Math.Truncate(number).ToLong();
 }
