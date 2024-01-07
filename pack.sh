47cc208cc8e44c25b1b36204c71cac83fb726374
Timerix22 <timerix11@gmail.com>
2024-01-07 20:58:37 +0600

    version dump and Publish configuration removal

--- pack.sh
+++ pack.sh
@@ -14,7 +14,7 @@ mkdir nuget
 function create_package() {
     echo "----------[$1]----------"
     cd "$1" || return 1
-    dotnet pack -c Publish -o bin/pack_tmp || return 1
+    dotnet pack -c Release -o bin/pack_tmp || return 1
     rm -rf bin/pack
     mv bin/pack_tmp bin/pack
     cp bin/pack/* ../nuget/
@@ -26,6 +26,8 @@ create_package DTLib
 create_package DTLib.Ben.Demystifier
 create_package DTLib.Dtsod
 create_package DTLib.Logging
+create_package DTLib.Logging.Microsoft
 create_package DTLib.Network
+create_package DTLib.XXHash
 
 ls -lh nuget
